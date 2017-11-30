bbf31580e25c947aed2018666742ded001f1e719
Faiz <faiz.ggmu@gmail.com>
2017-11-30 12:05:44 +0500

    Second Commit

---
+++ Assignment  31-35/app.js
@@ -0,0 +1,79 @@
+//Task 1
+// var currentDate =new Date();
+// alert(currentDate);
+//Task 2
+// var month = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+// var currentDate =new Date();
+// var currentMonth = currentDate.getMonth();
+// alert(month[currentMonth]);
+//Task 3
+// var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// var currentDate=new Date();
+// var currentDay=currentDate.getDay();
+// document.write("Today is "+days[currentDay]);
+//Task 4
+// var day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+// if(day[new Date().getDay()]=="Sunday"||day[new Date().getDay()]=="Saturday"){
+//     document.write("It's Fun Day!");
+// }
+// Task 5
+// var currentDate = new Date();
+// var currentDay = currentDate.getDate();
+// if(currentDay<16)
+// {
+// document.write("First fifteen days of the month");
+// }
+// else
+// {
+//     document.write("Last days of month");
+// }
+//Task 6
+// var currentDate=new Date();
+// document.write(currentDate);
+// document.write("<br>");
+// document.write("Ellapsed milliseconds since January 1,1970: " + (newDate.getTime()) + " <br />");
+// document.write("Ellapsed seconds since January 1,1970: " + ((newDate.getTime())/(1000*60*60) + " <br />"));
+// Task 7
+// if (new Date().getHours() > 12) {
+//     document.write("It's PM");
+// }
+// else {
+//     document.write("It's AM");
+// }
+// Task 8
+// var futureDate=new Date(2022,11,31);
+// alert(futureDate);
+// Task 9
+// var pastDate=new Date(2015,6,18);
+// var pastDays=pastDate.getTime();
+// var currentDate=new Date();
+// var day=currentDate.getTime();
+// document.write((new Date(day - pastDays).getTime() / (1000 * 60 * 60  * 24)).toFixed(0) + " days passed have passed since 1 Ramadan 2015");
+// Task 10
+// var referenceDay=new Date(2015,5,11);
+// var day=referenceDay.getTime();
+// alert(day);
+// var currentDate=new Date();
+// var currentDay=currentDate.getTime();
+// alert(currentDay);
+// alert((currentDay-day)/1000)
+// Task 11
+// var currentDate=new Date();
+// alert(currentDate);
+// var hour=currentDate.getHours()
+// alert(hour);
+// var onehourAhead=hour+1;
+// var ahead=new Date().setHours(onehourAhead);
+// alert(new Date(ahead));
+// Task 12
+// var curDate = new Date();
+// var hunderdYearAhead = new Date().setFullYear(curDate.getFullYear() - 100);
+// alert("current Date: " + curDate + " 100 years ago it was " + new Date(hunderdYearAhead));
+// Task 13
+// var age = prompt("enter your age");
+// var curYear = new Date();
+// var birthYear = new Date().setFullYear(curYear.getFullYear() - age);
+// document.write("your age is " + age + "<br />" + "your birth year is " + new Date(birthYear).getFullYear());
+//Task 14
+
+
